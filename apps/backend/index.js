e106e703573aaa0f25cc6c6ca3a3fd379b172840
morozov312 <alexander.morozov312@gmail.com>
2025-03-30 18:40:29 +0400

    feat: add base backend libs

--- apps/backend/index.js
+++ apps/backend/index.js
@@ -1,1 +1,4 @@
-const express = require('express'); const app = express(); app.get('/', (req, res) => res.send('Hello World')); app.listen(3000, () => console.log('Backend running on port 3000')); 
+const express = require('express')
+const app = express()
+app.get('/', (req, res) => res.send('Hello World'))
+app.listen(3000, () => console.log('Backend running on port 3000'))
