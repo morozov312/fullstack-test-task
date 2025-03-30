f2b31c8e502914c327aa3259b6d1374f0cd8e496
morozov312 <alexander.morozov312@gmail.com>
2025-03-30 21:40:54 +0400

    feat: frontend render posts

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -3,7 +3,6 @@ import postRoutes from './routes/postRoutes'
 import { POST_API_URL } from './constants'
 
 const app = express()
-
 app.use(express.json())
 app.use(POST_API_URL, postRoutes)
 
--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,13 +1,54 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import {
+  DxDataGridModule,
+  DxButtonModule,
+  DxTemplateModule,
+} from 'devextreme-angular'
+import { FormsModule } from '@angular/forms'
+
+const API_URL = '/api/posts'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [DxDataGridModule, DxButtonModule, DxTemplateModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  posts: any[] = []
+  newPost = {
+    title: '',
+    body: '',
+  }
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loadPosts()
+  }
+
+  loadPosts() {
+    this.http.get<any[]>(API_URL).subscribe((data) => (this.posts = data))
+  }
+
+  deletePost(event: any) {
+    const rowData = event.row.data
+    if (confirm('Удалить пост?')) {
+      this.http
+        .delete(`${API_URL}/${rowData.id}`)
+        .subscribe(() => this.loadPosts())
+    }
+  }
+
+  addPost() {
+    if (this.newPost.title && this.newPost.body) {
+      const newId = this.posts.length
+        ? this.posts[this.posts.length - 1].id + 1
+        : 1
+      this.posts.push({ ...this.newPost, id: newId })
+      this.newPost = { title: '', body: '' }
+    }
+  }
 }
